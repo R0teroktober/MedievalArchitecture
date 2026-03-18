e71c2b99d4403d0528dd930c103ad9db7f5beb9e
R0teroktober <st_bob_24@gmx.de>
2026-03-18 16:16:04 +0100

    Added: Rainsounds for windows + Fixed: window material type

--- MedievalArchitecture/MedievalArchitecture/src/Block/BlockMultiblockArchway.cs
+++ MedievalArchitecture/MedievalArchitecture/src/Block/BlockMultiblockArchway.cs
@@ -1,4 +1,5 @@
-﻿using Vintagestory.API.Common;
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 
@@ -8,6 +9,8 @@ namespace MedievalArchitecture
     public class BlockMultiblockArchway : BlockGeneric, IMultiBlockColSelBoxes
     {
         public ValuesByMultiblockOffset ValuesByMultiblockOffset { get; set; } = new();
+
+        ICoreClientAPI capi;
 
         public override void OnLoaded(ICoreAPI api)
         {
@@ -33,6 +36,17 @@ namespace MedievalArchitecture
             }
             Block originaBlock = blockAccessor.GetBlock(pos.AddCopy(offset.X, offset.Y, offset.Z));
             return originaBlock.GetSelectionBoxes(blockAccessor, pos);
+        }
+
+        public override float GetAmbientSoundStrength(IWorldAccessor world, BlockPos pos)
+        {
+            var conds = capi.World.Player.Entity.selfClimateCond;
+            if (conds != null && conds.Rainfall > 0.1f && conds.Temperature > 3f && (world.BlockAccessor.GetRainMapHeightAt(pos) <= pos.Y || world.BlockAccessor.GetDistanceToRainFall(pos, 3, 1) <= 2))
+            {
+                return conds.Rainfall;
+            }
+
+            return 0;
         }
     }
 }
